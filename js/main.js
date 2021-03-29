7cbfd49fa37188171dcb3b425dd64dcf05b74ad9
Cruser <someone@someplace.com>
2021-03-30 00:21:46 +0500

    add new files

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
-const searchLink = document.querySelector('.search-link');
-const searchBlock = document.querySelector('.header-search');
-const searchClose = document.querySelector('.header-search__close');
+const searchLink = document.querySelector('.search-link'),
+ searchBlock = document.querySelector('.header-search'),
+searchClose = document.querySelector('.header-search__close'),
+ menu = document.querySelector('.menu'),
+ burger = document.querySelector('.btn-menu'),
+ btnClose = document.querySelector('.menu__close');
+//  searchMenuOpen = document.querySelector('.search-link__close');
+
+
 searchLink.addEventListener('click', () => {
   searchBlock.style.display = 'block';
 });
@@ -8,3 +14,25 @@ searchLink.addEventListener('click', () => {
 searchClose.addEventListener('click', () => {
   searchBlock.style.display = 'none';
 });
+
+burger.addEventListener('click', () => {
+menu.classList.add('menu--visible');
+});
+
+btnClose.addEventListener('click', () => {
+menu.classList.remove('menu--visible');
+});
+
+burger.addEventListener('click', () => {
+  menu.classList.add('menu--visible');
+  });
+
+  searchLink.addEventListener('click', () => {
+    menu.classList.remove('menu--visible');
+    });
+
+    searchLink.addEventListener('click', () => {
+  searchBlock.style.display = 'block';
+});
+
+    
